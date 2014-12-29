7e2172b6d9df65c0f9dd6d2a46bd20fcab9aea8b
Gilad Raphaelli <g@raphaelli.com>
2014-12-29 12:35:07 -0500

    make syntax highlighting work with hl-line-mode

--- custom.el
+++ custom.el
@@ -1,5 +1,18 @@
 (custom-set-variables
- '(custom-enabled-themes (quote (wombat))))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(background-color "#002b36")
+ '(background-mode dark)
+ '(cursor-color "#839496")
+ '(custom-enabled-themes (quote (wombat)))
+ '(foreground-color "#839496")
+ '(save-place t nil (saveplace)))
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(flymake-errline ((t (:background "brightred" :foreground "yellow"))))
- )
+ '(hl-line ((t (:background "gray26")))))
